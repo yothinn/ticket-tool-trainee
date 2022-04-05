96210693c1eef870f2f5878bf94626f2e76247ec
Thanayut Phinsuwan <thanayut.p@g-able.com>
2022-04-05 18:27:01 +0700

    gen material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FilterButtonComponent } from './components/filter-button/filter-button.component';
 import { PaginatorComponent } from './components/paginator/paginator.component';
 import { StatusIconsComponent } from './components/status-icons/status-icons.component';
+import { MaterialModule } from './material/material.module';
 
 
 @NgModule({
@@ -16,11 +17,13 @@ import { StatusIconsComponent } from './components/status-icons/status-icons.com
     imports: [
         CommonModule,
         FormsModule,
+        MaterialModule,
         ReactiveFormsModule
     ],
     exports: [
         CommonModule,
         FormsModule,
+        MaterialModule,
         ReactiveFormsModule,
         // SharedModule
         FilterButtonComponent,
@@ -28,7 +31,6 @@ import { StatusIconsComponent } from './components/status-icons/status-icons.com
         StatusIconsComponent
 
     ]
-    
 })
 export class SharedModule
 {
