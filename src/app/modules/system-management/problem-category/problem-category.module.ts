54658f5050dfbe8fee0f66459bb89574ea53eb8f
Supriya Saengaun <supriyasaengaun1@gmail.com>
2022-04-22 18:26:15 +0700

    problem-category,components,my-ticket-info-view

--- src/app/modules/system-management/problem-category/problem-category.module.ts
+++ src/app/modules/system-management/problem-category/problem-category.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
+import { ProblemCategoryInfoComponent } from './components/problem-category-info/problem-category-info.component';
+import { ProblemCategoryListComponent } from './components/problem-category-list/problem-category-list.component';
 import { ProblemCategoryRoutingModule } from './problem-category-routing.module';
 import { ProblemCategoryComponent } from './problem-category/problem-category.component';
 
 
 
+
 @NgModule({
   declarations: [
-    ProblemCategoryComponent
+    ProblemCategoryComponent,
+    ProblemCategoryInfoComponent,
+    ProblemCategoryListComponent
   ],
   imports: [
     ProblemCategoryRoutingModule,
