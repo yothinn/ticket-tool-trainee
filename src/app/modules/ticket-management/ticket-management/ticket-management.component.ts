08898f6a7846a4291048a3f476fa73f6b9af7404
Yothin Setthachatanan <yothin.s@g-able.com>
2022-05-04 14:16:11 +0700

    Fix ticket filter

--- src/app/modules/ticket-management/ticket-management/ticket-management.component.ts
+++ src/app/modules/ticket-management/ticket-management/ticket-management.component.ts
@@ -4,6 +4,7 @@ import { PageResponse } from 'app/core/base/pageResponse.types';
 import { GetTicketParameter } from 'app/core/parameters/getTicketParameter.entity';
 import { TicketService } from 'app/core/ticket/ticket.service';
 import { Ticket } from 'app/core/ticket/ticket.types';
+import { FilterButton } from 'app/shared/components/filter-button/filter-button-interface';
 import { Observable } from 'rxjs';
 import { TicketCardListComponent } from '../components/ticket-card-list/ticket-card-list.component';
 
@@ -19,6 +20,22 @@ export class TicketManagementComponent implements OnInit {
   @ViewChild('ticketListComp') ticketListComp: TicketCardListComponent;
 
   ticketResponse$?: Observable<PageResponse<Ticket[]>>;
+
+  filters: FilterButton[] = [
+    { name: 'All', total: 100 },
+    { name: 'wait for approve', total: 10 },
+    { name: 'wait for reassign', total: 5 },
+    { name: 'assigned', total: 5 },
+    { name: 'processing', total: 5 },
+    { name: 'closed', total: 5 },
+    { name: 'unresolved', total: 5 },
+    { name: 'cancelled', total: 5 },
+  ];
+  currFilter?: FilterButton = this.filters[0];
+
+  myTaskFilters: FilterButton[] = [
+    { name: 'My Task', total: 0 },
+  ];
 
   isViewMode: boolean = false;
 
@@ -50,6 +67,14 @@ export class TicketManagementComponent implements OnInit {
     this.drawerDetail.toggle();
     this._ticketService.activeTicket = undefined;
     this.ticketListComp.activeTicket = undefined;
+  }
+
+  onStatusFilter(filter: FilterButton): void {
+
+  }
+
+  onMyTaskFilter(filter: FilterButton): void {
+    console.log(filter);
   }
 
   private _openDetail(): void {
