456269518579adea612613d891aa053c789f66d4
Supriya Saengaun <supriyasaengaun1@gmail.com>
2022-05-06 18:03:04 +0700

    dialog request data

--- src/app/modules/problem-management/request-data/dialogs/request-data-edit-dialog/request-data-edit-dialog.component.ts
+++ src/app/modules/problem-management/request-data/dialogs/request-data-edit-dialog/request-data-edit-dialog.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RequestData } from 'app/core/request-data/request-data.types';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { values } from 'lodash';
 
 @Component({
   selector: 'app-request-data-edit-dialog',
@@ -12,26 +16,60 @@ export class RequestDataEditDialogComponent implements OnInit {
   requestData?: RequestData;
   // isNew: boolean = true;
 
-  controlType = 'textbox';
+  requestForm?: FormGroup;
+
   request: string = '';
+
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  requestValue:string[]; 
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private _data: any,
     private _dialogRef: MatDialogRef<RequestDataEditDialogComponent>,
+    private _formBuilder: FormBuilder,
   ) { }
-
-  ngOnInit(): void {
-    this.requestData = this._data;
-    // this.isNew = (this.requestData) ? false : true;
-    this.request = this.requestData?.keyName;
-
-    console.log(this._data);
-  }
 
   get isNew(): boolean {
     return (this.requestData) ? false : true;
   }
 
+  ngOnInit(): void {
+    this.requestData = this._data;
+    // this.isNew = (this.requestData) ? false : true;
+    this.request = this.requestData?.keyName;
+    this.requestForm = this.initRequestDataForm(this._data);
+
+    console.log(this._data);
+
+  }
+
+  initRequestDataForm(request:RequestData): FormGroup {
+    return this._formBuilder.group({
+      keyName: [request?.keyName || '', Validators.required],
+      displayName: [request?.displayName || '', Validators.required],
+      controlType: [request?.controlType, Validators.required],
+
+    });
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.requestValue.push(value);
+    }
+
+    event.chipInput!.clear();
+  }
+
+  remove(requestValue: string): void {
+    const index = this.requestValue.indexOf(requestValue);
+
+    if (index >= 0) {
+      this.requestValue.splice(index, 1);
+    }
+  }
 
   close(): void {
     this._dialogRef.close(undefined);
@@ -42,3 +80,5 @@ export class RequestDataEditDialogComponent implements OnInit {
   }
 
 }
+
+
