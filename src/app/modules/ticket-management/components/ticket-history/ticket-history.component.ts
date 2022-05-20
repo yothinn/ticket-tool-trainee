7ce2cccd04fef9f493b73d262c3c322acb8c099e
Thanayut Phinsuwan <thanayut.p@g-able.com>
2022-05-20 17:22:33 +0700

    vertical timeline

--- src/app/modules/ticket-management/components/ticket-history/ticket-history.component.ts
+++ src/app/modules/ticket-management/components/ticket-history/ticket-history.component.ts
@@ -7,11 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketHistoryComponent implements OnInit {
 
-  completed = true;
-
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
